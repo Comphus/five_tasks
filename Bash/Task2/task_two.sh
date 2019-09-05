201958702faffc30ba50cba56f7cabe161a748ef
Gabriel Lopez <gabiota3000@yahoo.com>
2019-09-05 03:25:21 -0400

    updated .sh file

--- Bash/Task2/task_two.sh
+++ Bash/Task2/task_two.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 key="`cat key.txt`"
 url="https://www.alphavantage.co/query"
-echo $key
-res=$(curl -X POST -H "Content-Type: application/json" \
---data '{ "function":"TIME_SERIES_MONTHLY","symbol":"MSFT", "apikey":"$key" }' \
-$url\
-exit 0)
-
-echo $res
+script="import requests
+params = {'function':'TIME_SERIES_MONTHLY','symbol':'MSFT', 'apikey':'$key'}
+with requests.get('$url', params=params) as r:
+	results = r.json()
+	results = list(results['Monthly Time Series'].items())[:3]
+	print(results)
+"
+python -c "$script"
